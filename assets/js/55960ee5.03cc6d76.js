649132dca33d1749fe3bba8f78a92f1437e1f0bf
Mikita Kukavenka <nkukovenko@by.byelex.com>
2022-12-02 18:06:40 +0300

    Deploy website - based on 951c606df92786e60ff9202fe26d696f6b91a8d4

---
+++ assets/js/55960ee5.03cc6d76.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmordheim_assault=self.webpackChunkmordheim_assault||[]).push([[121],{8070:function(e){e.exports=JSON.parse('[{"name":"page","permalink":"/docs/tags/page","count":1}]')}}]);
---
+++ assets/js/b2b675dd.25178e72.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmordheim_assault=self.webpackChunkmordheim_assault||[]).push([[533],{8017:function(e){e.exports=JSON.parse('{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
