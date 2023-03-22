b78cf202e5ecbee2ba949364cd4fa5660a1d29ce
Mikita Kukavenka <godinfrog@gmail.com>
2023-03-22 17:33:22 +0300

    Added classes

--- src/components/character-generator/character-description/talents.jsx
+++ src/components/character-generator/character-description/talents.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
+const divStyle = {
+  border: '1px solid var(--ifm-color-emphasis-300)',
+  boxShadow: '0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23)',
+  padding: '10px'
+};
+
 function Talents(props) {
   return (
-    <div>
+    <div style={divStyle}>
       <h3>{props.name.toUpperCase()} TALENTS</h3>
       <table>
         <thead>
--- src/components/character-generator/character-generator.jsx
+++ src/components/character-generator/character-generator.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
 import CharacterDescription from './character-description/character-description'
+import Term from './character-description/term';
+import CharacterImg from './character-description/character-img/character-img';
 
 const races = require('./character-options.json')
 
@@ -11,9 +13,14 @@ function CharacterGenerator(props) {
       <Tabs className="unique-tabs">
         {races.race.map((race, i) => (
           <TabItem value={race.value} label={race.name} default={i === 0}>
+            {race.image ? (<CharacterImg src={race.image}></CharacterImg>) : ('')}
+            {race.fluff ? (<p><i>{race.fluff}</i></p>) : (<p>NO FLUFF</p>)}
+            {race.requirement ? (<Term name="Requirements:">{race.requirement}</Term>) : ('')}
+            {race.language ? (<Term name="">{race.language}</Term>) : (<p>NO LANGUAGE</p>)}
+            {race.raceTalent ? (<Term name={race.raceTalent.name}>{race.raceTalent.description}</Term>) : (<p>NO RACE TALENT</p>)}
             <Tabs className="unique-tabs">
               {race.class.map((cl, j) => (
-                <TabItem value={cl.value} label={cl.name} default={j === 0}>                  
+                <TabItem value={cl.value} label={cl.name} default={j === 0}>
                   <CharacterDescription cl={cl} race={race.value}></CharacterDescription>
                 </TabItem>)
               )}
