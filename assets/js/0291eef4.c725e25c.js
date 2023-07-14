06fcc6285dae5bdcbb09edce37d75c84a37cc22b
Mikita Kukavenka <nkukovenko@by.byelex.com>
2023-07-14 15:31:22 +0300

    Deploy website - based on 649cb3dab45cb3e7233dae0aea5152ca588d8783

--- assets/js/0291eef4.c725e25c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkosr=self.webpackChunkosr||[]).push([[6772],{4469:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
--- assets/js/3504d203.21fc1fa3.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkosr=self.webpackChunkosr||[]).push([[9038],{5745:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
